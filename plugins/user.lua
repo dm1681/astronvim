04cce0a389c0c9387271da9a01c582b418eb1100
dm1681 <dm1681@gmail.com>
2023-06-13 09:32:23 -0700

    themes

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+      style = "storm",
+      transparent = true
+    },
+}
 }
