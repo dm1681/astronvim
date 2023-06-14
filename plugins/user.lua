91df63bc6ab45de8a8f20a3c74f800d9de4bfc5a
dm1681 <dm1681@gmail.com>
2023-06-13 18:22:15 -0700

    "add smooth scrolling"

--- plugins/user.lua
+++ plugins/user.lua
@@ -17,5 +17,18 @@ return {
       style = "storm",
       transparent = true
     },
+  }, 
+  {
+    "declancm/cinnamon.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() require('cinnamon').setup{
+      default_keymaps = true,
+      extra_keymaps = true,
+      extended_keymaps = true,
+      override_keymaps = true,
+      always_scroll = true,
+    } end
   }
 }
+
